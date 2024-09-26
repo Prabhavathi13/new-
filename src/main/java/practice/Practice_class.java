b02946ec1c7ab5e87988d4b3f2179e2ecd06939a
Prabhavathi13 <merimiprabha99@gmail.com>
2024-09-26 19:41:28 +0530

    added maven project

---
+++ src/main/java/practice/Practice_class.java
@@ -0,0 +1,11 @@
+package practice;
+
+public class Practice_class {
+
+	public static void main(String[] args) {
+		String a = "Hello World!" ;
+		System.out.println(a);
+		  
+	}
+
+}
---
+++ src/test/resources/TestClass.java
@@ -0,0 +1,13 @@
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		@Test
+		public void testClass() {
+			System.out.println("This is test class");
+		}
+		
+
+	}
+
+}
